b38533a74fae4f4ab83993074aba5c87ba41aee1
mimitch2 <mike@melodix.com>
2018-05-02 10:03:08 -0500

    got pos working

--- server/index.js
+++ server/index.js
@@ -5,18 +5,16 @@ const products = require("./products");
 const vehicles = require("./vehicles");
 const contacts = require("./contacts");
 
-// console.log(comments[1]["_id"]);
 
-
-
-app.listen(3002, (err) => {
+app.listen(3001, (err) => {
   if (err) {
     return console.log("Error", err);
   } 
-  console.log("Web server is now running on port 3002");
+  console.log("Web server is now running on port 3001");
 });
 
 
+// <-----------get full arrays------------->
 app.get("/comments",function (req,res) {
   return res.json(comments);
 });
@@ -35,20 +33,17 @@ app.get("/contacts",function (req,res) {
 
 
 
-
-
+// <-----------find specific id------------->
 const findObject = (arr, key, data) => {
   return arr.find((index) => {
     return index[key] === data;
   });
 };
 
-
 app.get("/comments/:id",function (req,res) {
   const id = Number(req.params.id);
   return res.json(findObject(comments, "_id", id));
 });
-
 
 app.get("/products/:id",function (req,res) {
   const id = Number(req.params.id);
@@ -65,25 +60,31 @@ app.get("/contacts/:id",function (req,res) {
   return res.json(findObject(contacts, "_id", id));
 });
 
+
+
+// <-----------post something------------->
+const postSomething = (arr) => {
+  return arr.push({name: "Mike"});
+};
+
+app.post("/comments",function (req,res) {
+  return res.json(postSomething(comments));
+});
+
+app.post("/products",function (req,res) {
+  return res.json(postSomething(products));
+});
+
+app.post("/vehicles",function (req,res) {
+  return res.json(postSomething(vehicles));
+});
+
+app.post("/contacts",function (req,res) {
+  return res.json(postSomething(contacts));
+});
   
 
-
-
-
-
-
-
-
-
-app.post("/users",function (req,res) {
-  return res.json(users.push({name: "Mike"}));
-});
-
-app.delete("/users",function (req,res) {
-  return res.json(users.pop());
-});
-
-
+// <-----------handle bad request------------->
 app.use(function (request,response) {
   response.send("NOPE!!!!");
 });
