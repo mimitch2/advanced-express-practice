e73aa6c04b89f0b5e36b2b1e34abff2a1afb3bf2
mimitch2 <mike@melodix.com>
2018-05-11 10:29:49 -0500

    update before branching

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ app.listen(3001, (err) => {
   console.log("Web server is now running on port 3001`");
 });
 
-
 // <-----------handle bad request------------->
 app.use(function (request,response) {
   response.send("NOPE!!!!");
