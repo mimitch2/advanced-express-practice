51b37c7f0b1af49de1723925a60bb3de526ec2a5
mimitch2 <mike@melodix.com>
2018-05-11 15:04:48 -0500

    got all 4 working, but now need to replace the individual id lookup to point to the db and not the array

--- server/controllers/VehiclesController.js
+++ server/controllers/VehiclesController.js
@@ -1,19 +1,37 @@
 const Vehicle = require( "../models/vehicles.js");
+let vehicles = [];
+
+Vehicle.find({}).exec()
+.then(commentsArr => {
+  vehicles = [...commentsArr];
+});
 
 module.exports.list = function list(request, response) {
+  Vehicle.find({}).exec()
+.then(vehiclesArr => {
+  vehicles = [...vehiclesArr];
   return response.json(vehicles);
+});
+  console.log(vehicles);
 };
+
 module.exports.show = function show(request, response) {
   const id = Number(request.params.id);
   return response.json(vehicles.find(index => index["_id"] === id));
 };
+
 module.exports.create = function create(request, response) {
   const input = request.body;
-  const newObj = 
-    {_id: vehicles.length + 1, imgUrl: null, year: input.year, make: input.make, 
-      model: input.model, price: null, km: null, miles: null, fuel: null, city: null, isNew: null};
-  return response.json(vehicles.push(newObj));
+  const newVehicle = new Vehicle(
+    {_id: vehicles.length + 1, year: input.year + " ", make: input.make + " ", 
+      model: input.model}
+      );
+  newVehicle.save().then(savedVehicle => {
+    vehicles.push(savedVehicle);
+    return response.json(savedVehicle);
+  });
 };
+
 module.exports.update = function update(request, response) {
   return response.json({theId: request.params.id});
 };
